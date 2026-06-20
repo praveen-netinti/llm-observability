61368d57bfb40cf436be1cd61a9093bd8666cf53
praveen-kumar-netinti <praveennetinti2002@gmail.com>
2026-06-20 17:33:15 +0530

    style(resizable): fix focus classnames

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -29,7 +29,10 @@ function ResizableHandle({
     <ResizablePrimitive.Separator
       data-slot='resizable-handle'
       className={cn(
-        "bg-faded-lighter focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
+        "bg-faded-lighter relative flex w-px items-center justify-center",
+        "after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2",
+        "focus-visible:bg-faded-light focus-visible:outline-hidden",
+        "aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
         className,
       )}
       {...props}

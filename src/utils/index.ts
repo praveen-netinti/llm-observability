3f2b54e71fe01f695691f243a0a21df49706a7d4
praveen-kumar-netinti <praveennetinti2002@gmail.com>
2026-06-20 10:22:50 +0530

    feat: add utility functions (cn, tv, polymorphic types, recursive clone)

---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+// Core utilities
+export { cn, type ClassValue } from './cn';
+export { tv, type VariantProps } from './tv';
+export { recursiveCloneChildren } from './recursive-clone-children';
+
+// Type utilities
+export type {
+  PolymorphicRef,
+  PolymorphicComponent,
+  PolymorphicComponentProps,
+  PolymorphicComponentPropsWithRef,
+} from './polymorphic';
